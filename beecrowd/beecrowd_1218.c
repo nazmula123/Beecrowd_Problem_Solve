e9a90c95c0baed29d6c91d100ad473057237a781
Md. Nazmul Islam Hridoy <waheddr1@gmail.com>
2024-07-07 00:04:53 +0600

    Add files via upload

---
+++ beecrowd/beecrowd_1218.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+int a,b;
+char ch;
+int count=0,count1=0;
+while (scanf("%d %d %c",&a,&b,&ch)!=EOF)
+{
+
+    int count=0,count1=0;
+   if(a ==b && ch=='F')
+
+    count++;
+    else if(a ==b && ch=='M')
+ count1++;
+
+
+}
+
+printf("%d",count);
+
+}
+
+
+
+
+
+
+
+}
